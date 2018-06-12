9ab81dcfacf0595ab3e3b121c73c4c58ffca6139
MagicalMoonY <37144001+MagicalMoonY@users.noreply.github.com>
2018-06-12 13:01:54 +0800

    Update Date.cpp

--- Date.cpp
+++ Date.cpp
@@ -1,7 +1,12 @@
 class Date {
 public:
-  Date(int y, int m, int d);
-  ~Date();
+  Date(int y, int m, int d)
+  {
+     cout<<“12356749235253426865629856262846”<<endl;
+  };
+  ~Date(){
+      cout << "结构"<<endl;
+  };
 
   void nextDay();
   void previousDay();
@@ -12,7 +17,21 @@ private:
   int month;
   int day;
 };
-
+void nextDay(){
+    day++;
+}
+void previousDay(){
+  day--;
+}
+bool equal(Date other){
+    if(day == 30){
+        return 1;
+    }
+  return 0;
+}
+void show(){
+    cout << day << endl;
+}
 int main() {
   Date today(2012,12,20);
   Date day(2012,12,20);
