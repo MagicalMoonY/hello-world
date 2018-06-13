341f55fc133a984af3e3cd74d353954884e3a2e5
MagicalMoonY <37144001+MagicalMoonY@users.noreply.github.com>
2018-06-13 15:23:30 +0800

    Update ClassConstructor.cpp

--- ClassConstructor.cpp
+++ ClassConstructor.cpp
@@ -1,2 +1,20 @@
-class {
+#ifndef CLASSCONSTRUCTOR_H_
+#define CLASSCONSTRUCTOR_H_
+#include<string>
+class Stock
+{private:
+	std::string company;
+	long shares;
+	double share_val;
+	double total_val;
+	void set_tot(){
+		total_val = shares * share_val;
+	}
+public:
+	Stock(const std::string & co,long n,double pr);
+	void buy(long num,double price);
+	void sell(long num,double price);
+	void update(double price);
+	void show();
 };
+#endif
