3689b743f01cb9ad3a5b2adc6f3d1e9daaef6208
MagicalMoonY <37144001+MagicalMoonY@users.noreply.github.com>
2018-06-13 15:27:07 +0800

    Update Date.cpp

--- Date.cpp
+++ Date.cpp
@@ -1,12 +1,11 @@
+#include<iostream> 
+using namespace std;
+
+int a[13]={0,31,28,31,30,31,30,31,31,30,31,30,31};
 class Date {
 public:
-  Date(int y, int m, int d)
-  {
-     cout<<“12356749235253426865629856262846”<<endl;
-  };
-  ~Date(){
-      cout << "结构"<<endl;
-  };
+  Date(int y, int m, int d);
+//  ~Date();
 
   void nextDay();
   void previousDay();
@@ -17,24 +16,72 @@ private:
   int month;
   int day;
 };
-void nextDay(){
-    day++;
+
+Date::Date(int y,int m,int d)
+{
+	year=y;
+	month=m;
+	day=d;
 }
-void previousDay(){
-  day--;
+
+void Date::nextDay()
+{
+	if(year%400==0||year%100!=0&&year%4==0)
+	a[2]=29;
+	else
+	a[2]=28;	
+	if(day<a[month])
+	day++;
+	else if(day==a[month]&&month==12)
+	{
+		day=1;
+	    month=1;
+	    year++;
+	}
+	else
+	{
+		day=1;
+		month++;
+	}
 }
-bool equal(Date other){
-    if(day == 30){
-        return 1;
-    }
-  return 0;
+
+void Date::previousDay()
+{
+	if(year%400==0||year%100!=0&&year%4==0)
+	a[2]=29;
+	else
+	a[2]=28;
+	if(day>1)
+	day--;
+	else if(day==1&&month==1)
+	{
+		month=12;
+		day=31;
+		year--;
+	}
+	else
+	{
+		month--;
+		day=a[month];
+	}
 }
-void show(){
-    cout << day << endl;
+
+void Date::show()
+{
+	cout<<year<<"Äê"<<month<<"ÔÂ" <<day<<"ÈÕ"<<endl;
 }
+
+bool Date::equal(Date other)
+{
+	if(other.day==day&&other.month==month&&other.year)
+	return true;
+	else
+	return false;
+}
+
 int main() {
-  Date today(2012,12,20);
-  Date day(2012,12,20);
+  Date today(2018,3,20);
+  Date day(2018,3,20);
   day.show();
   for(int i=0;i<30;i++)
     day.nextDay();
