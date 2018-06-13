771cfa50fbdfe009021e60513ce03205c90a6c74
MagicalMoonY <37144001+MagicalMoonY@users.noreply.github.com>
2018-06-13 15:06:11 +0800

    Update ConstructExpress.cpp

--- ConstructExpress.cpp
+++ ConstructExpress.cpp
@@ -1,35 +1,56 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class Vehicle{
+class Euclidean{
 	public:
-		Vehicle(int w,int wei){//基类构造函数，用于初始化基类数据 
-			wheels = w;
-			weight = wei;
+		Euclidean(long long int a, long long int n) {
+			this->a = a;
+			this->n = n;
 		}
-		void Show(void) const{
-		    cout <<"轮胎数:"<<wheels<<"车重:"<<weight<<"kg"<<endl;
+		~Euclidean() {}
+		
+		void solve_equation() {
+			int j, p = a, q = n, r, s = 0;//p除数 q被除数 r商 s余数 
+			for (j = 0; s!= 1; j++) {
+				r = q / p;
+				b[j] = r;
+				s = q % p;
+				q = p;
+				p = s;
+			}
+
+			int t = j;
+			j--;
+			c[0] = 1;
+			c[1] = b[j];
+		j--;
+			int i;
+			for (i = 2; j >= 0; i++) {
+				c[i] = c[i - 2] + c[i - 1] * b[j];
+				j--;
+			}
+			i--;
+			if (t % 2) {
+				c[i] = n - c[i]; 
+			}
+			cout << c[i] << endl;
 		}
+		
+		
 	private:
-		int wheels;//轮数
-		int weight;//车重 
-}; 
-class Car : public Vehicle{//定义派生类 
-	public:
-		Car(int w,int wei,int p):Vehicle(w,wei){//派生类构造函数初始化列表 
-			passenger_load = p;//初始化派生类新增数据成员 
-		}
-/*		派生类名（参数表） ：基类名1（参数表1），。。。，基类名n(参数表n) {//如果基类定义了无参数的构造函数，或者由编译器自动生成的默认构造函数时，在派生类构造函数的定义时可以省略对基类构造函数的调用，即在初始化列表中省略基类名（参数表） 
-			派生类新增成员的初始化 
-		}*/
-		void Show(void) const {
-		    cout <<"此轿车最多能载客："<<passenger_load<<"人"<<endl;//访问本类私有数据成员 
-		    Vehicle :: Show();//访问基类同名成员函数Show(),需要使用作用域限定符 
-		}
-    private:
-    	int passenger_load;
+		int b[100000];//商 
+		int c[100000];//中间值 
+		long long int a, n;
 };
-int main(){
-	Car car(4,5000,5);//创建一个派生类对象 
-	car.Show();
+
+int main() {
+	long long int a, n; //a除数 n被除数 
+	while(1) {
+		cin >> a >> n;
+		if (a == 0 && n == 0) {
+			break;
+		}
+		Euclidean Ecalgo(a, n);
+		Ecalgo.solve_equation();
+	}
 	return 0;
 }
