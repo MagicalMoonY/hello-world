6969908568c9bf212e2745e084a81193961e5167
MagicalMoonY <37144001+MagicalMoonY@users.noreply.github.com>
2018-06-13 15:29:42 +0800

    Update ObjectLife.cpp

--- ObjectLife.cpp
+++ ObjectLife.cpp
@@ -1,4 +1,11 @@
-class {
-};
+#include<iostream>
+#include"ConstructExpress.cpp"
 int main() {
+	{
+		using std::cout;
+		cout << "Using constructors to create new objiects\n";
+		Stock stock1("NanoSmart",12,20.0);
+		stock1.show();
+	}
+	return 0;
 }
